a80092670d780c65b4e802ffd414ba3d15aed7ba
Pumpon4yk <tt10392@gmail.com>
2023-02-17 18:12:12 +0100

    task01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { Container } from './App.styled';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Statistics from './Statistics';
+
+export class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    OPT = ['Good', 'Neutral', 'Bad'];
+
+    onClickBtn = e => {
+        const value = e.target.value;
+        this.setState(prevState => {
+            return { ...prevState, [value]: prevState[value] + 1 };
+        });
+    };
+
+    countTotalFeedback = () => {
+        const data = this.state;
+        return Object.values(data).reduce((c, v) => c + v, 0);
+    };
+
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state;
+        const total = this.countTotalFeedback();
+        const percent = Math.round((good / total) * 100);
+
+        return percent ? percent : 0;
+    };
+
+    render() {
+        const total = this.countTotalFeedback();
+        const countPercent = this.countPositiveFeedbackPercentage();
+        const { good, neutral, bad } = this.state;
+
+        return (
+            <Container>
+                <Section
+                    title="Please leave feedback"
+                    children={
+                        <FeedbackOptions
+                            options={this.OPT}
+                            onLeaveFeedback={this.onClickBtn}
+                        />
+                    }
+                />
+
+                <Section
+                    title="Statistic"
+                    children={
+                        <Statistics
+                            good={good}
+                            neutral={neutral}
+                            bad={bad}
+                            total={total}
+                            positivePercentage={countPercent}
+                        />
+                    }
+                />
+            </Container>
+        );
+    }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ContainerBtn = styled.div`
+display: flex;
+gap: 20px;
+`;
+
+
+export const Button = styled.button`
+padding: 5px 10px;
+background-color: white;
+border: 1px solid tomato;
+border-radius: 5px;
+
+:hover{
+    background-color: #3399ff;
+}
+`;
